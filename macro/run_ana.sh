561eee16f44edf67b2f004e91c882429e8b40f3d
Mariia Mitrankova <mashalario@gmail.com>
2026-07-01 16:00:07 -0400

    Fun4All to run on my DSTs

---
+++ macro/run_ana.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${LOGNAME}
+
+
+source /opt/sphenix/core/bin/sphenix_setup.sh new
+
+#printenv
+
+nEvents="$1"
+runnumber=79513
+segment="$2"
+nSkip="$3"
+
+OutDir=/sphenix/tg/tg01/hf/mitrankova/PatternReco/79513
+
+echo ========================================================
+echo Running Fun4All_TPC_Tracking.C  
+echo "  nEvents             = ${nEvents}"
+echo "  nSkip               = ${nSkip}"
+echo "  Run Number          = ${runnumber}"
+echo "  Segment             = ${segment}"
+echo "  OutDir              = ${OutDir}"
+echo ========================================================
+
+
+ root.exe -l -b << EOF
+        .x Fun4All_TPC_Tracking.C(${nEvents},${runnumber}, ${segment},"${OutDir}",${nSkip})
+EOF
+
+echo all done
